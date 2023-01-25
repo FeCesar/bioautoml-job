9cc71ac8a64f645f1650ae53c81dc5ee6f5c0081
Felipe Cesar Rampazzo <ff.cc.ss.rr@hotmail.com>
2023-01-25 18:31:20 -0300

    Transformando json em objeto

--- src/init_process_service.py
+++ src/init_process_service.py
@@ -1,12 +1,22 @@
 import logging
 import base64
+import json
+from types import SimpleNamespace
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def start(process):
-    print(decode(process))
+def start(message):
+    decoded_message = decode(message)
+    process = json.loads(decoded_message, object_hook=lambda d: SimpleNamespace(**d))
+
+    print(process)
+    print(process.processModel)
+    print(process.parametersEntity)
+
+    for file in process.files:
+        print(file)
 
 
 def decode(process):
