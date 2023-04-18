ea61b97c415d162914bc5493446f19183e2632b8
Felipe Cesar Sepulvida Rampazzo <ff.cc.ss.rr@gmail.com>
2023-04-18 13:34:35 -0300

    Adicionando data na atualização de status

--- src/modules/classes/UpdateStatusVO.py
+++ src/modules/classes/UpdateStatusVO.py
@@ -1,10 +1,12 @@
 import json
+from datetime import datetime
 
 
 class UpdateStatusVO:
     def __init__(self, status, process_id):
         self.processId = process_id
         self.status = status
+        self.referenceDate = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     def to_json(self):
         return json.dumps(self.__dict__)
