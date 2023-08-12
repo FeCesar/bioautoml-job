5fadc9e4eb2899c99ac5036e7489c6de4f5c8a21
Felipe Cesar Sepulvida Rampazzo <ff.cc.ss.rr@gmail.com>
2023-08-12 20:09:31 -0300

    CD Bugfix

--- setup.sh
+++ setup.sh
@@ -11,23 +11,9 @@ git pull
 git submodule init
 git submodule update
 
-# Install miniconda
-cd ~
-
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-
-chmod +x Miniconda3-latest-Linux-x86_64.sh
-
-./Miniconda3-latest-Linux-x86_64.sh
-
-export PATH=~/miniconda3/bin:$PATH
-
-# Create environment
-conda env create -f BioAutoML-env.yml -n bioautoml
-
 # Run the job
 apt-get update -y
-apt-get update -y
+apt-get upgrade -y
 apt-get install -y python3-pip
 pip install -r ~/bioautoml-job/requirements.txt
 python3 ~/bioautoml-job/src/main.py
