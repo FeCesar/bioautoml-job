6721da0df2b08fd157fab9393cb68348e6f55e1f
Felipe Cesar Sepulvida Rampazzo <ff.cc.ss.rr@gmail.com>
2023-08-13 14:05:36 -0300

    Bugfix

--- setup.sh
+++ setup.sh
@@ -16,4 +16,4 @@ apt-get update -y
 apt-get upgrade -y
 apt-get install -y python3-pip
 pip install -r ~/bioautoml-job/requirements.txt
-source /env/environment && python3 ~/bioautoml-job/src/main.py
+source /etc/environment && python3 ~/bioautoml-job/src/main.py
