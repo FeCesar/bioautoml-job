e867899c8403676d3415743c673d6e7f6d15a46f
Felipe Cesar Sepulvida Rampazzo <ff.cc.ss.rr@gmail.com>
2023-08-13 16:59:47 -0300

    Bugfix

--- src/modules/services/logger_service.py
+++ src/modules/services/logger_service.py
@@ -1,7 +1,4 @@
 from os import environ
-from os import path
-from os import makedirs
-from datetime import datetime
 import logging
 
 
@@ -10,14 +7,7 @@ def get_logger(name):
     log_formatter = "[%(asctime)s] [%(module)s.%(funcName)s:%(lineno)d] [thread=%(thread)d] " \
                     "[%(levelname)s] [%(message)s]"
 
-    directory = "logs"
-    if not path.exists(directory):
-        makedirs(directory)
-
-    now = datetime.now()
-    now = now.strftime("%d-%m-%YT%H-%M-%S")
-
-    log_filename = f"./logs/app-{now}.log"
+    log_filename = "app.log"
 
     logging.basicConfig(filename=log_filename, filemode='w', format=log_formatter, level=log_level)
 
