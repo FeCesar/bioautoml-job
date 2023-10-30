574828ff0303d026c37e6f3719f6c26226eec97b
Felipe Cesar Sepulvida Rampazzo <ff.cc.ss.rr@gmail.com>
2023-10-30 00:10:46 -0300

    Melhorando logs

--- src/modules/services/logger_service.py
+++ src/modules/services/logger_service.py
@@ -1,16 +1,17 @@
 from os import environ
 import logging
+import json_log_formatter
 
 
 def get_logger(name):
     log_level = environ.get("APP_LOGGER_LEVEL")
-    log_formatter = "[%(asctime)s] [%(module)s.%(funcName)s:%(lineno)d] [thread=%(thread)d] " \
-                    "[%(levelname)s] [%(message)s]"
 
-    log_filename = "app.log"
-
-    logging.basicConfig(filename=log_filename, filemode='w', format=log_formatter, level=log_level)
+    formatter = json_log_formatter.VerboseJSONFormatter()
+    json_handler = logging.FileHandler(filename='/app.json')
+    json_handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
+    logger.addHandler(json_handler)
+    logger.setLevel(log_level)
 
     return logger
